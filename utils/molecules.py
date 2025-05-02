a9b77cbac0275406471b7b056d3c888d4ab3cd08
drgretchenboria <me@gretchenboria.com>
2025-05-02 12:36:32 -0700

    Mock up of two tools
    2D image generation decoupled from fetching smiles code.

--- utils/molecules.py
+++ utils/molecules.py
@@ -1,17 +1,27 @@
 from rdkit.Chem import Draw
 from rdkit import Chem
+import os
 
 
-def generate_molecule_images(medications, output_format):
-    for name, smiles in medications:
-        img = Draw.MolToImage(Chem.MolFromSmiles(smiles))
-        img.save(f'{name}.{output_format}')
+def get_smiles_from_name(name):
+    sample_meds = [
+        ('lamotrigine', 'C1=CC(=C(C(=C1)Cl)Cl)C2=C(N=C(N=N2)N)N'),
+        ('citalopram', 'CN(C)CCCC1(C2=C(CO1)C=C(C=C2)C#N)C3=CC=C(C=C3)F'),
+        ('phenobarbital', 'CC1=CC=CC=C1OC(=O)N1C=CN1C(=O)C2C=CC=CC=C2')
+    ]
+    for med_name, smiles in sample_meds:
+        if med_name == name:
+            return smiles
+    return None
 
 
-sample_meds = [
-    ('lamotrigine', 'C1=CC(=C(C(=C1)Cl)Cl)C2=C(N=C(N=N2)N)N'),
-    ('citalopram', 'CN(C)CCCC1(C2=C(CO1)C=C(C=C2)C#N)C3=CC=C(C=C3)F')
-]
+def generate_molecule_images(drug_name, drug_smiles, output_format='png', save_dir='molecule_images'):
+    os.makedirs(save_dir, exist_ok=True)
+    img = Draw.MolToImage(Chem.MolFromSmiles(drug_smiles))
+    img.save(os.path.join(save_dir, f'{drug_name}.{output_format}'))
 
-generate_molecule_images(sample_meds, 'png')
 
+test_drug_name = 'lamotrigine'
+citalopram_smiles = get_smiles_from_name(test_drug_name)
+if citalopram_smiles is not None:
+    generate_molecule_images(test_drug_name, drug_smiles=citalopram_smiles)
